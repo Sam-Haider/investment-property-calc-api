f4ef64e935845e3d4f8b87e42ef273bc72cd3770
SHaider0 <Samnoon.Haider@gmail.com>
2021-04-08 21:35:42 -0400

    connect to mongodb cluster

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require("express")
 const app = express()
 const dotenv = require("dotenv").config()
 const cors = require("cors")
+const mongoose = require("mongoose")
 
 app.use(cors({ origin: [process.env.CLIENT_ORIGIN, 'http://localhost:3000'] }))
+
+const uri = `mongodb+srv://sam:${process.env.DB_PASSWORD}@cluster1.hyibd.mongodb.net/investmentpropcalcdb?retryWrites=true&w=majority`
+
+mongoose.connect(uri, { useNewUrlParser: true }, { useUnifiedTopology: true })
+    .then(() => console.log("connected to mongoDB"))
+    .catch((e) => console.log("whoops, issue connecting to mongoDB:", e))
 
 const PORT = process.env.PORT || 8081
 
