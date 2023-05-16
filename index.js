4bd6ad6382a6e17af8ab39d62ce038f358360418
Sam-Haider <sammhaider0@gmail.com>
2023-05-15 23:25:33 -0400

    Update mongodb connection string

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
-const express = require("express")
-const app = express()
-require("dotenv").config()
-const cors = require("cors")
-app.use(cors({ origin: [process.env.CLIENT_ORIGIN, "http://localhost:3000"] }))
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const cors = require("cors");
+app.use(cors({ origin: [process.env.CLIENT_ORIGIN, "http://localhost:3000"] }));
 
-
-const mongoose = require("mongoose")
-const propertiesRoutes = require("./routes/properties")
-const bodyParser = require("body-parser")
-app.use(bodyParser.json())
+const mongoose = require("mongoose");
+const propertiesRoutes = require("./routes/properties");
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const uri = `mongodb+srv://sam:${process.env.DB_PASSWORD}@cluster0.ifdiz.mongodb.net/`;
 
-const uri = `mongodb+srv://sam:${process.env.DB_PASSWORD}@cluster1.hyibd.mongodb.net/investmentpropcalcdb?retryWrites=true&w=majority`
+mongoose
+  .connect(uri, { useNewUrlParser: true }, { useUnifiedTopology: true })
+  .then(() => console.log("connected to mongoDB"))
+  .catch((e) => console.log("whoops, issue connecting to mongoDB:", e));
 
-mongoose.connect(uri, { useNewUrlParser: true }, { useUnifiedTopology: true })
-    .then(() => console.log("connected to mongoDB"))
-    .catch((e) => console.log("whoops, issue connecting to mongoDB:", e))
+app.use("/", propertiesRoutes);
 
-app.use("/", propertiesRoutes)
-
-const PORT = process.env.PORT || 8081
+const PORT = process.env.PORT || 8081;
 
 app.listen(PORT, function () {
-    console.log(`Server is running on port ${PORT}`)
-})
+  console.log(`Server is running on port ${PORT}`);
+});
