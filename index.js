d09b4b94a779e95c205d5b6ad84b0a457190e391
Sam-Haider <sammhaider0@gmail.com>
2023-05-17 15:49:16 -0400

    Fix access control issue for client

--- index.js
+++ index.js
@@ -9,6 +9,18 @@ const propertiesRoutes = require("./routes/properties");
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function (req, res, next) {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://main--investment-property-calc.netlify.app"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 const uri = `mongodb+srv://sam:${process.env.DB_PASSWORD}@cluster0.ifdiz.mongodb.net/`;
 
